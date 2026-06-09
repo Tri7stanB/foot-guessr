a300c18f27250235523547939efb663f94605d08
Tristan Bart <t.bart@openium.fr>
2026-06-09 22:19:08 +0200

    initial commit

---
+++ src/main/java/com/tbart/foot_guessr/FootGuessrApplication.java
@@ -0,0 +1,13 @@
+package com.tbart.foot_guessr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootGuessrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootGuessrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbart/foot_guessr/FootGuessrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbart.foot_guessr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootGuessrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
